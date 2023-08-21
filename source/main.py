273f48efabacbf104597617686c7c9975ae5075c
Diego <diegopsrx0001@gmail.com>
2023-08-21 15:25:34 -0700

    Actually add some functionality

--- source/main.py
+++ source/main.py
@@ -1,19 +1,28 @@
 import discord
+from loguru import logger
+
 
 class Sparkasse:
     intents = None
 
     class MyClient(discord.Client):
         async def on_ready(self):
-            print(f'Logged on as {self.user}!')
+            logger.debug(f"Successfully logged in as {self.user}")
 
         async def on_message(self, message):
-            print(f'Message from {message.author}: {message.content}')
+            if message.author == self.user:
+                return
+            
+            if message.content.startswith('$send'):
+                await message.channel.send('Sending money!')
     
     def setup_intents(message_content_intent: bool) -> None:
         Sparkasse.intents = discord.Intents.default()
         Sparkasse.intents.message_content = message_content_intent
 
     def execute_client(token: str) -> None:
+        if Sparkasse.intents == None or Sparkasse.intents == False:
+            logger.debug("Intents have been disabled!")
+            
         client = Sparkasse.MyClient(intents=Sparkasse.intents)
         client.run(token)
