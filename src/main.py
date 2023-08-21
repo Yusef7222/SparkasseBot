6ed4f165a2a45da89aac393ca8b3c12c3d7564eb
Diego <diegopsrx0001@gmail.com>
2023-08-21 14:54:49 -0700

    Added main.py and run.py
    Simple example taken from the documentation and added a .gitignore for data

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import discord
+
+import json
+import sys
+
+class Sparkasse:
+    intents = None
+
+    class MyClient(discord.Client):
+        async def on_ready(self):
+            print(f'Logged on as {self.user}!')
+
+        async def on_message(self, message):
+            print(f'Message from {message.author}: {message.content}')
+    
+    def setup_intens(message_content_intent: bool) -> None:
+        Sparkasse.intents = discord.Intents.default()
+        Sparkasse.intents.message_content = message_content_intent
+
+    def execute_client(token: str) -> None:
+        client = Sparkasse.MyClient(intents=Sparkasse.intents)
+        client.run(token)
